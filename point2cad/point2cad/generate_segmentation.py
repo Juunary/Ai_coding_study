66a612d33060f3cfcdcec6a9028bd0204d336db4
June woo Kang <harry020104@naver.com>
2025-03-23 21:36:23 +0900

    .

--- point2cad/point2cad/generate_segmentation.py
+++ point2cad/point2cad/generate_segmentation.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     parser = argparse.ArgumentParser(description="ParseNet Segmentation Prediction")
-    parser.add_argument("--path_in", type=str, default="./assets/abc_00321.xyz")
+    parser.add_argument("--path_in", type=str, default="./assets/abc_00323.xyz")
     parser.add_argument("--with_normals", type=bool, default=False)
     cfg = parser.parse_args()
 
